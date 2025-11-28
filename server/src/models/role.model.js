932a147d218b111f89dcc56d91042f8dcb851f3d
rajeshgu <rajeshlkh2020@gmail.com>
2025-11-29 02:52:36 +0530

    add Role schema with permissions and validation

---
+++ server/src/models/role.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const roleSchema = new mongoose.Schema({
+  name: {
+    type: String, required: true, unique: true
+  },
+  description: String,
+  permissions: [{ type: String }]
+},
+  { timestamps: true }
+);
+
+export default mongoose.model("Role", roleSchema);
